d5c423b5c4a78054d110c5180157e9c9f1828627
Yuri Katayama <tooketsu@gmail.com>
2019-01-25 20:10:21 -0200

    cu

--- Assets/Scripts/OldMan.cs
+++ Assets/Scripts/OldMan.cs
@@ -14,15 +14,48 @@ public class OldMan : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		age = 4;
-		jump = 0;
-		speed = 0.5f;
-		
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		AgeChanger (age);
 		
 	}
+
+	void AgeChanger (float playerAge) {
+		
+		if (age == 1) {
+			speed = 0.5f;
+			jump = 0;
+			details = 1;
+
+		}
+
+		if (age == 2) {
+			speed = 0.8f;
+			jump = 0.8f;
+			details = 0.75f;
+		}
+		if (age == 3) {
+			speed = 1f;
+			jump = 1;
+			details = 0.5f;
+
+		}
+
+		if (age == 4) {
+			speed = 1.5f;
+			jump = 1.5f;
+			details = 0.25f;
+		}
+		
+
+	}
+
+
 }
+
+
