a1a031e61e34df6869967ac9ead0d37207e996fd
Yuri Katayama <tooketsu@gmail.com>
2019-01-25 20:01:18 -0200

    oi

---
+++ Assets/Scripts/OldMan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldMan : MonoBehaviour {
+
+	private int age;
+	private float jump;
+	private float speed;
+	private float details;
+	
+	// private float miopia;
+	// private float stress;
+
+	// Use this for initialization
+	void Start () {
+		age = 4;
+		jump = 0;
+		speed = 0.5f;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
