25d5eea5b678a18690429330a5b6a09ce6a50cbe
Yuri Katayama <tooketsu@gmail.com>
2019-01-25 23:47:42 -0200

    plyaeractions

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerActions : MonoBehaviour {
+public static class PlayerActions {
 
 	public static bool alimentarPombos = false;
 	public static bool oculos = false;
 	public static bool chave = false;
 	public static float timer = 0;
 	public static int contador = 0;
-	public static 
+	public static bool eventCollider = true;
 
 	/*
 	public static bool 
@@ -22,23 +22,13 @@ public class PlayerActions : MonoBehaviour {
 	public static bool 
 	*/
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void ContarPombos () {
+	public static void ContarPombos () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			contador++;
 		}
 	}
 
-	void TimeCounter () {
+	public static void TimeCounter () {
 		timer += 1 * Time.deltaTime;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 	}
 }
