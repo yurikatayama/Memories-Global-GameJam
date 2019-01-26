ca1ff3e247be7ce3e5cd568378f4f00836fc6371
Yuri Katayama <tooketsu@gmail.com>
2019-01-25 23:09:08 -0200

    riariaria

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour {
+
+	public static bool alimentarPombos = false;
+	public static bool oculos = false;
+	public static bool chave = false;
+	public static float timer = 0;
+	public static int contador = 0;
+	public static 
+
+	/*
+	public static bool 
+	public static bool 
+	public static bool 
+	public static bool 
+	public static bool 
+	public static bool 
+	public static bool 
+	public static bool 
+	*/
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ContarPombos () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			contador++;
+		}
+	}
+
+	void TimeCounter () {
+		timer += 1 * Time.deltaTime;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+	}
+}
