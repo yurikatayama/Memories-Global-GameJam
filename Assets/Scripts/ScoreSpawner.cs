897458b806436d22c5e85d3838e772b50cedb49f
edualb <eduardoslafiq@gmail.com>
2019-01-26 10:37:35 -0200

    Spawn again

--- Assets/Scripts/ScoreSpawner.cs
+++ Assets/Scripts/ScoreSpawner.cs
@@ -10,7 +10,7 @@ public class ScoreSpawner : MonoBehaviour {
 	public float spawnTimer = 0;
 	// Use this for initialization
 	void Start () {
-		spawnTime = 3f;
+		spawnTime = 2f;
 	}
 	
 	// Update is called once per frame
@@ -18,9 +18,10 @@ public class ScoreSpawner : MonoBehaviour {
 	void FixedUpdate () {
 		spawnTimer += 1 * Time.deltaTime;
 
-		if (spawnTimer > 1) {
-			spawnTimer -= 0.15f * Time.deltaTime;
-		}
+		/* if (spawnTimer > 1) {
+			//spawnTimer -= 0.15f * Time.deltaTime;
+			spawnTimer = 0;
+		}*/
 
 		if (spawnTimer > spawnTime) {
 			Spawn ();
@@ -30,7 +31,7 @@ public class ScoreSpawner : MonoBehaviour {
 
 		void Spawn ()
      {
-         spawnPoint.x = 10;
+         spawnPoint.x = Random.Range(12, 30);
          spawnPoint.y = Random.Range(-1, 2);
          Instantiate(objectScore[Random.Range(0, objectScore.Length - 1)], spawnPoint, Quaternion.identity);
      }
---
+++ Assets/Scripts/SpawnAction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SpawnAction {
+
+	public static List<Vector2> arrayV2;
+
+	public static void iniciaArrayV2 () {
+		arrayV2 = new List<Vector2>();
+	}
+
+    /* public static void setXY () {
+        contador = 0;
+    }*/
+}
