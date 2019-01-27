377b5004b9c010f8d315d99cbeb0dba78952bdd1
Yuri Katayama <tooketsu@gmail.com>
2019-01-27 13:46:27 -0200

    Creditos nice

---
+++ Assets/Creditos Resources/EndCredits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCredits : MonoBehaviour {
+
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += 1 * Time.deltaTime;
+
+		if (timer >= 28.9f) {
+			SceneManager.LoadScene(0);
+		}
+		
+	}
+}
